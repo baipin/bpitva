6219234cf3cde5776b32631a8a86eabe3fc16250
baipin <39830783+baipin@users.noreply.github.com>
2021-03-22 14:36:49 +0800

    Add files via upload

---
+++ 20210322-new.py
@@ -0,0 +1,11 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print"hello world"
+SyntaxError: invalid syntax
+>>> print "hello world"
+SyntaxError: Missing parentheses in call to 'print'
+>>> print 'hello world'
+SyntaxError: Missing parentheses in call to 'print'
+>>> print ('hello world')
+hello world
+>>> 
